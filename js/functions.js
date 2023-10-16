80f3c1aff173d9990d95e906d8308daf7b13978f
ZhurmanJulia <zhurmanjulia@mail.ru>
2023-10-17 09:01:42 +1000

    Добавит функцию для проверки палиндромов

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,20 @@
+//Функция проверки длины строки
 const fitsStringLength = (text, maxLength) => text.length <= maxLength;
 
+//Тесты для проверки функции длины строки
 fitsStringLength('проверяемая строка', 20);
 fitsStringLength('проверяемая строка', 18);
 fitsStringLength('проверяемая строка', 10);
+
+
+//Функция для проверки, является ли строка палиндромом.
+const isPalindrome = (sequence) => {
+  const text = String(sequence).replaceAll(' ', '').toLowerCase();//приводим строку к нормализованному виду: без пробелов и в строчном регистре
+  const reversedText = text.split('').reverse().join('');// Нужно перевернуть строку. Но такого метода у строк нет. Поэтому мы превращаем строку в массив (.split('')), переворачиваем ее (.reverse()), а потом обратно преобразуем в строку (.join(''))
+  return text === reversedText;
+};
+
+//Тесты для проверки функции длины строки
+isPalindrome('топот');//true
+isPalindrome('ДовОд');//true
+isPalindrome('Кекс');//false

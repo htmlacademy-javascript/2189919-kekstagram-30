fa7a1746eec97cdb508641597aac35f3d91682d9
ZhurmanJulia <zhurmanjulia@mail.ru>
2023-10-17 10:31:03 +1000

    Добавлят функцию извлечения  цифр

--- js/functions.js
+++ js/functions.js
@@ -2,9 +2,9 @@
 const fitsStringLength = (text, maxLength) => text.length <= maxLength;
 
 //Тесты для проверки функции длины строки
-fitsStringLength('проверяемая строка', 20);
-fitsStringLength('проверяемая строка', 18);
-fitsStringLength('проверяемая строка', 10);
+fitsStringLength('проверяемая строка', 20);//true
+fitsStringLength('проверяемая строка', 18);//true
+fitsStringLength('проверяемая строка', 10);//false
 
 
 //Функция для проверки, является ли строка палиндромом.
@@ -14,7 +14,18 @@ const isPalindrome = (sequence) => {
   return text === reversedText;
 };
 
-//Тесты для проверки функции длины строки
+//Тесты для проверки функции является ли строка палиндромом.
 isPalindrome('топот');//true
 isPalindrome('ДовОд');//true
 isPalindrome('Кекс');//false
+
+const parseDigits = (sequence) => {
+  const digits = String(sequence).replace(/[^0-9]+/g, ''); // replace() - заменить что-то. // - разделители регулярного выражения. [] - группировка(диапозон цифр, группа). + - 1 или более совпадение (1 или более цифр). g - заменить все(greedy - жадный поиск). "" - на что мы хотим заменить. ^ - все кроме того, что мы указали в скобках. Все, что кроме цифр - убери
+  return digits ? Number(digits) : NaN;
+};
+
+parseDigits('2023 год'); // 2023
+parseDigits('ECMAScript 2022'); // 2022
+parseDigits('1 кефир, 0.5 батона'); // 105
+parseDigits('агент 007'); // 7
+parseDigits('а я томат'); // NaN

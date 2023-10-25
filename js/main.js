37ea34c1a720b9aa70437262f1d17e09ac75dac3
ZhurmanJulia <zhurmanjulia@mail.ru>
2023-10-25 10:22:47 +1000

    Исправляет неточности

--- js/main.js
+++ js/main.js
@@ -11,25 +11,25 @@ const getRandomInteger = (min, max) => {
 
 const createCommentsData = (itemCount) => {
   const messages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+  const names = ['Маша', 'Паша', 'Саша', 'Марина', 'Оля', 'Катя', 'Иван', 'Даниил', 'Костя', 'Таня', 'Сергей', 'Лера', 'Соня'];
 
   return new Array (itemCount).fill(1).map((start, index) => ({
     id: start + index,
-    message: getRandomItem(messages)
+    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    message: getRandomItem(messages),
+    name: getRandomItem(names),
   }));
 };
 
 const createPicturesData = (itemCount = 25) => {
   const descriptions = ['Зарядитесь нашим теплом.', 'Я хочу быть с тобой до последней страницы.', 'Ты – причина моего счастья.', 'Это я в Дубае и я щас отдыхаю.'];
-  const names = ['Маша', 'Паша', 'Саша', 'Марина', 'Оля', 'Катя', 'Иван', 'Даниил', 'Костя', 'Таня', 'Сергей', 'Лера', 'Соня'];
 
   return new Array (itemCount).fill(1).map((start, index) => ({
     id: start + index,
     url: `photos/${start + index}.jpg`,
     description: getRandomItem(descriptions),
     likes: getRandomInteger(15, 200),
-    comments: createCommentsData(getRandomInteger(0, 30)),
-    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-    name: getRandomItem(names)
+    comments: createCommentsData(getRandomInteger(0, 30))
   }));
 };
 
